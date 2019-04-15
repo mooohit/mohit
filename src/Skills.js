2cae80c8a8ec2b37c397fdf3c8786594720fe441
Mohit <bestmohit.99@gmail.com>
2019-04-15 15:07:54 +0530

    skills button size

--- src/Skills.js
+++ src/Skills.js
@@ -18,42 +18,42 @@ export default function Skills(){
                         <div className="one1">
                            
                             <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" title="Javacript" target="blank" className="" >
-                            <FontAwesomeIcon icon={['fab', 'js-square']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fab', 'js-square']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                             <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" title="HTML" className="" target="blank">
-                            <FontAwesomeIcon icon={['fab', 'html5']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fab', 'html5']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                         </div>
                         <div className="one2">
                             <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" title="CSS" className="" target="blank">
-                            <FontAwesomeIcon icon={['fab', 'css3']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fab', 'css3']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                             <a href="https://www.python.org/about/" title="Python" className="" target="blank">
-                            <FontAwesomeIcon icon={['fab', 'python']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fab', 'python']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                         </div>
                     </div>
                     <div className="two">  
                         <div className="two1"> 
                             <a href="https://moz.com/learn/seo/what-is-seo" title="SEO" target="blank" className="" >
-                            <FontAwesomeIcon icon={['fas', 'search']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fas', 'search']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                             <a href="https://en.wikipedia.org/wiki/Search_engine_marketing" title="SEM"  target="blank" className="" >
-                            <FontAwesomeIcon icon={['fas', 'chart-line']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fas', 'chart-line']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                             {/* <a href="https://www.geeksforgeeks.org/c-language-set-1-introduction/" target="blank" className="" >
-                            <FontAwesomeIcon icon={['fas', 'code']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fas', 'code']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                             <a href="https://www.geeksforgeeks.org/java/" className="" target="blank">
-                            <FontAwesomeIcon icon={['fab', 'java']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fab', 'java']}  size='5x' color="#49494b"   className="w"/> 
                             </a> */}
                         </div>
                         <div className="two1">
                             <a href="https://reactjs.org/" title="React" className="" target="blank">
-                            <FontAwesomeIcon icon={['fab', 'react']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fab', 'react']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                             <a href="https://nodejs.org/" title="NodeJS" className="" target="blank">
-                            <FontAwesomeIcon icon={['fab', 'node']}  size='6x' color="#49494b"   className="w"/> 
+                            <FontAwesomeIcon icon={['fab', 'node']}  size='5x' color="#49494b"   className="w"/> 
                             </a>
                         </div>
                     </div>
